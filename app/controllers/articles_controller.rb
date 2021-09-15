43bb339011703dd6f20557336056abf73825a041
SizweMalobola <25reyals@gmail.com>
2021-09-15 20:03:37 +0200

    edit & update controllers are functional

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,9 +14,22 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(user_params)
         if @article.save
-            redirect_to article_path(@article)
+            redirect_to @article
         else
             render 'new'
+        end
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(user_params)
+            redirect_to @article
+        else
+            render 'edit'
         end
     end
 
